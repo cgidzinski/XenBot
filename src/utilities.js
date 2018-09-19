d2bd1ded464dd302f02d51e57af2afad5de83da7
Colin Gidzinski <evg31337@gmail.com>
2018-09-18 21:13:30 -0400

    Hey match

--- src/utilities.js
+++ src/utilities.js
@@ -1,5 +1,6 @@
 var config = require('./config.json');
 var stringSimilarity = require('string-similarity');
+let trigger = "xenbot"
 
 function isAdmin(msg) {
     return config.admin.includes(msg.author.id)
@@ -14,57 +15,51 @@ function getRole(message) {
 }
 
 function message(msg) {
-    if (msg.content.trim().toLowerCase().includes("xenbot")) {
-        command(msg)
-    } else {
-        speech(msg)
+    let strings = msg.content.trim().toLowerCase().split(" ")
+    let sim = stringSimilarity.findBestMatch(trigger, strings);
+    if (sim.bestMatch.rating >= 0.50) {
+        console.log("\n---------------------------------------------")
+
+        console.log("COMMAND MATCHED [" + sim.bestMatch.rating + "]")
+        let user = getRole(msg)
+        let cmd = strings.join(" ")
+        let filteredCmd = strings.filter(word => {
+            return word != sim.bestMatch.target
+        }).join(" ")
+        console.log("-- User: " + user.name)
+        console.log("-- Admin:", user.admin)
+        console.log("-- Bot:", user.bot)
+        console.log("-- Command:", cmd)
+
+        matchList(msg, filteredCmd)
+        console.log("---------------------------------------------")
     }
 }
 
-function speech(message) {
-    let user = getRole(message)
-    console.log("[" + user.name + "] " + message.content)
-    return
-}
+function match(strings, cmd, message, callback) {
+    let sim = stringSimilarity.findBestMatch(cmd, strings);
+    console.log("Comparing [" + cmd + "] to [" + strings + "]")
+    console.log("Sim Match Percent: " + sim.bestMatch.target + " @ " + sim.bestMatch.rating)
+    let matchedCount = 0
+    for (var i = 0; i < strings.length; i++) {
+        if (cmd.includes(strings[i])) {
+            matchedCount += 1
+        }
+    }
+    let matchedPercent = matchedCount / cmd.split(" ").length
+    console.log("Word Match Percent: " + matchedPercent)
 
-function match(string, message, callback) {
-    let text = message.content.trim().toLowerCase().replace("xenbot", "").trim()
-    let sim = stringSimilarity.findBestMatch(text, string);
-    console.log("Comparing " + text + " to " + string)
-    console.log("Best Match: " + sim.bestMatch.target + " " + sim.bestMatch.rating)
-    if (sim.bestMatch.rating >= 0.50) {
+    if (sim.bestMatch.rating >= 0.50 || (sim.bestMatch.rating >= 0.30 && matchedPercent >= 0.30)) {
         return callback(true)
     }
-
     message.reply('Im not sure what you said....');
     return callback(false)
-    // 
-
 }
 
-function command(message) {
-    let user = getRole(message)
-    let cmd = message.content.trim().toLowerCase()
-    console.log("COMMAND ------------------------------")
-    console.log("[" + user.name + "] ")
-    console.log("-- Admin:", user.admin)
-    console.log("-- Bot:", user.bot)
-    console.log("-- Command:", cmd)
-    console.log("COMMAND ------------------------------")
-
-    match(["hey", "hi"], message, (success => {
-        if (success) {
-            message.reply('Hey');
-        }
+function matchList(msg, cmd) {
+    match(["hey", "hi", "hello", "sup", "konichiwa"], cmd, msg, (success => {
+        if (success) msg.reply("Hey there :)");
     }))
-
-
-
-
-
-
-
-    return
 }
 
 module.exports = {
